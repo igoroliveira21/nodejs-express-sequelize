532b4061e0dad5e7d93fdb10d06cdd9979949ec1
igoroliveira21 <igoron182017@gmail.com>
2023-12-19 23:12:01 -0300

    Projeto desenvolvido durante o curso de ORM com Node.js da Alura

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const pessoas = require('./pessoasRoute.js');
+const categorias = require('./categoriaRoute.js');
+const cursos = require('./cursoRoute.js');
+
+
+module.exports = app => {
+  app.use(
+    express.json(),
+    pessoas,
+    categorias,
+    cursos
+  );
+};
