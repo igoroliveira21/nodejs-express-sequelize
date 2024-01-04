6c3e5042540e99aa1dcae3e0922877538c87a3cd
igoroliveira21 <igoron182017@gmail.com>
2024-01-03 21:27:20 -0300

    Criando uma rota para buscar cursos pela data inicial e final

--- src/controllers/CursoController.js
+++ src/controllers/CursoController.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const Controller = require('./Controller.js');
 const CursoServices = require('../services/CursoServices.js');
 
@@ -6,6 +7,26 @@ const cursoServices = new CursoServices();
 class CursoController extends Controller {
   constructor() {
     super(cursoServices);
+  }
+
+  async pegaCursos(req, res) {
+    const {data_inicial, data_final} = req.query;
+    const where = {};
+
+    // SE EXISTIREM OS PARAMS, CRIAR UMA PROPRIEDADE {}
+    data_inicial || data_final ? where.data_inicio = {} : null;
+    //SE EXISTIR DATA INICIAL, ADICIONA A PROP gte com o valor
+    data_inicial ? where.data_inicio[Op.gte] = data_inicial : null;
+    //Se EXISTIR DATA FINAL, idem
+    data_final ? where.data_inicio[Op.lte] = data_final : null;
+
+    try {
+      const listaCursos = await cursoServices.pegaTodosOsRegistros(where);
+      return res.status(200).json(listaCursos);
+    } catch (erro) {
+      return res.status(500).json({erro: erro.messagem});
+    }
+
   }
 }
 
